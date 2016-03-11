2bf44afb3a47df733bb6b084cbb0675564efde08
MangoSister <yyp0502@gmail.com>
2016-03-11 17:44:37 -0500

    optimize edge flip

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -628,11 +628,6 @@ namespace CMU462
 		 HalfedgeIter h4 = h3->next();
 		 HalfedgeIter h5 = h4->next();
 		 
-		 HalfedgeIter h6 = h1->twin();
-		 HalfedgeIter h7 = h2->twin();
-		 HalfedgeIter h8 = h4->twin();
-		 HalfedgeIter h9 = h5->twin();
-		 
 		 //Vertices
 		 VertexIter v0 = h0->vertex();
 		 VertexIter v1 = h3->vertex();
@@ -652,48 +647,22 @@ namespace CMU462
 		 
 		 //Edges
 		 //e0 is given
-		 EdgeIter e1 = h1->edge();
-		 EdgeIter e2 = h2->edge();
-		 EdgeIter e3 = h4->edge();
-		 EdgeIter e4 = h5->edge();
-		 
 
 		 //2. reassign elements
 		 
 		 //HalfEdges
-		 h0->next() = h1; h0->twin() = h3; h0->vertex() = v3; h0->edge() = e0; h0->face() = f0;
-		 
-		 h1->next() = h2; h1->twin() = h7; h1->vertex() = v2; h1->edge() = e2; h1->face() = f0;
-		 
-		 h2->next() = h0; h2->twin() = h8; h2->vertex() = v0; h2->edge() = e3; h2->face() = f0;
-		 
-		 h3->next() = h4; h3->twin() = h0; h3->vertex() = v2; h3->edge() = e0; h3->face() = f1;
-		 
-		 h4->next() = h5; h4->twin() = h9; h4->vertex() = v3; h4->edge() = e4; h4->face() = f1;
-		 
-		 h5->next() = h3; h5->twin() = h6; h5->vertex() = v1; h5->edge() = e1; h5->face() = f1;
-		 
-			//exterior
-		 h6->twin() = h5; h6->vertex() = v2; h6->edge() = e1;
-		 
-		 h7->twin() = h1; h7->vertex() = v0; h7->edge() = e2;
-		 
-		 h8->twin() = h2; h8->vertex() = v3; h8->edge() = e3;
-		 
-		 h9->twin() = h4; h9->vertex() = v1; h9->edge() = e4;
-		 
+		 h0->next() = h5; h0->vertex() = v2;
+		 h5->next() = h1; h5->face() = f0;
+		 h1->next() = h0;
+		 h3->next() = h2; h3->vertex() = v3;
+		 h2->next() = h4; h2->face() = f1;
+		 h4->next() = h3;
+		
 		 //Vertices
-		 v0->halfedge() = h2;
-		 v1->halfedge() = h5;
-		 v2->halfedge() = h3;
-		 v3->halfedge() = h0;
-		 
-		 //Edges
-		 e0->halfedge() = h0;
-		 e1->halfedge() = h5;
-		 e2->halfedge() = h1;
-		 e3->halfedge() = h2;
-		 e4->halfedge() = h4;
+		 v0->halfedge() = h4;
+		 v1->halfedge() = h1;
+		 v2->halfedge() = h0;
+		 v3->halfedge() = h3;
 		 
 		 //Faces
 		 f0->halfedge() = h0;
